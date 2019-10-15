468d6854a14007dfae7f9f6a2cfd01fa9a92ac04
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-10-15 11:30:59 +0200

    U5 Step 23

---
+++ andfun-kotlin-guess-it/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.guesstheword.screens.game
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(val finalScore: Int): ViewModel() {
+    init {
+
+    }
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
